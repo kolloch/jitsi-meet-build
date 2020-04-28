ebf4c8342dcdb408f0fa244ab0ab71b11551f132
Peter Kolloch <info@eigenvalue.net>
2020-04-28 16:25:54 +0200

    This is not really a commit message.

--- default.nix
+++ default.nix
@@ -3,57 +3,80 @@
 , lib ? pkgs.lib
 , nodejs ? pkgs.nodejs-12_x
 , sources ? import nix/sources.nix
-, jitsiMeetSrc ?
-    # if you check out your sources in a parallel directory,
-    # they are automatically picked up.
-    if builtins.pathExists ../jitsi-meet
-    then
-        let path = ../jitsi-meet;
-        in builtins.trace "DEV MODE: Using jitsi-meet sources from ${builtins.toString path}" path
-    else sources.jitsi-meet
+  # If you check out the jitsi-meet sources into ../jitsi-meet,
+  # they are automatically picked up.
+, jitsiMeetSrc ? if builtins.pathExists ../jitsi-meet
+  then
+    let
+      path = ../jitsi-meet;
+    in
+      builtins.trace "DEV MODE: Using jitsi-meet sources from ${builtins.toString path}" path
+  else sources.jitsi-meet
+, gitignoreSrc ? sources.gitignore
+, gitignore ? pkgs.callPackage gitignoreSrc {}
 }:
 
 rec {
   /* Derives a package including static files and the minified/"compiled" libs. */
   package =
+    assert lib.canCleanSource jitsiMeetSrc;
     let
-      dirs = pkgs.linkFarm "jitsi-meet-libs" [
-        { name = "libs"; path = internal.libs; }
-        {
+      subDir = name: "${builtins.toString jitsiMeetSrc}/${name}";
+
+      # Copy some directories verbatim into destination.
+      staticDirs =
+        let subDirInStore = name:
+            builtins.path {
+              # We want to prevent putting all of the jitsiMeetSrc dir into
+              # the store but we DO want to put the sub directory into the store.
+              path = subDir name;
+              inherit name;
+            };
+          toSubPathName = name: { inherit name; path = subDirInStore name; };
+        in
+        builtins.map toSubPathName [
+          "connection_optimization"
+          "fonts"
+          "images"
+          "static"
+          "sounds"
+          "lang"
+        ];
+
+      processedDirs =
+        let filteredSubDir = name: allowedPatterns:
+          assert builtins.isString name;
+          assert builtins.isList allowedPatterns;
+          {
+            inherit name;
+            path = lib.sourceByRegex (subDir name) allowedPatterns;
+          };
+        in
+        [
+          { name = "libs"; path = internal.libs; }
           # This should also be built and not simply copied
-          name = "css";
-          path =
-            lib.sourceByRegex
-              "${builtins.toString jitsiMeetSrc}/css"
-              [ "all.css" ];
-        }
-        {
-          name = "resources";
-          path =
-            lib.sourceByRegex
-              "${builtins.toString jitsiMeetSrc}/resources"
-              [ "robots.txt" ];
-        }
-      ];
+          (filteredSubDir "css" [ "all.css" ])
+          (filteredSubDir "resources" [ "robots.css" ])
+        ];
+      dirs = pkgs.linkFarm "jitsi-meet-dirs" (staticDirs ++ processedDirs);
       staticFiles = lib.sourceByRegex jitsiMeetSrc [
         ''[^.].*\.js''
         ''.*\.html''
-        "connection_optimization"
         "favicon.ico"
-        "fonts"
-        "images"
-        "static"
-        "sounds"
         "LICENSE"
-        "lang"
       ];
-    in
-      pkgs.symlinkJoin {
-        name = "jitsi-meet-package";
+      joined = pkgs.symlinkJoin {
+        name = "jitsi-meet-symlinked";
         paths = [ staticFiles dirs ];
       };
+      # Deep copy, ensure no dangling symlinks
+    in
+      pkgs.runCommand "jitsi-meet-package" {} ''
+        cp -RL ${joined} $out
+      '';
 
   internal = {
+    # Does not work current. :(
     # libs = internal.libsViaNode2Nix;
     libs = internal.libsViaCopy;
 
@@ -95,18 +118,25 @@ rec {
 
     /* Returns the cleaned sources for the jtisi-meet libs. */
     libsSrc =
-        assert lib.canCleanSource jitsiMeetSrc;
-        let precleaned = lib.cleanSource jitsiMeetSrc;
-            prune = name: type:
-                let baseName = baseNameOf (toString name); in ! (
-                    (type == "directory" && builtins.elem baseName [
-                        "node_modules"
-                        "ios"
-                        "android"
-                        "libs"
-                    ])
-                );
-            pruned = lib.cleanSourceWith { filter = prune; src = precleaned; };
-        in pruned;
+      assert lib.canCleanSource jitsiMeetSrc;
+      let
+        precleaned = lib.cleanSource jitsiMeetSrc;
+        prune = name: type:
+          let
+            baseName = baseNameOf (toString name);
+          in
+            ! (
+              (
+                type == "directory" && builtins.elem baseName [
+                  "node_modules"
+                  "ios"
+                  "android"
+                  "libs"
+                ]
+              )
+            );
+        pruned = lib.cleanSourceWith { filter = prune; src = precleaned; };
+      in
+        gitignore.gitignoreSource pruned;
   };
 }
